b2ff205f3b3c12dc8889c2575036a691f1fa3745
cetdelosaltos <6967398+cetdelosaltos@users.noreply.github.com>
2024-08-31 15:39:24 -0400

    seguimos actualizando ahora el boot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NgbModule],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
